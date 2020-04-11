594ae34ba71a32dce0b832163070008ed1890273
Alejandro Cayssials <info@webxander.com>
2020-04-11 05:54:10 -0300

    update themes

---
+++ resources/views/templates/eshop/layout.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<!-- Meta Tag -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name='copyright' content=''>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Title Tag  -->
+    <title>Eshop - eCommerce HTML5 Template.</title>
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="/eshop/images/favicon.png">
+	<!-- Web Font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+	
+	<!-- StyleSheet -->
+	
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="/eshop/css/bootstrap.css">
+	<!-- Magnific Popup -->
+    <link rel="stylesheet" href="/eshop/css/magnific-popup.min.css">
+	<!-- Font Awesome -->
+    <link rel="stylesheet" href="/eshop/css/font-awesome.css">
+	<!-- Fancybox -->
+	<link rel="stylesheet" href="/eshop/css/jquery.fancybox.min.css">
+	<!-- Themify Icons -->
+    <link rel="stylesheet" href="/eshop/css/themify-icons.css">
+	<!-- Nice Select CSS -->
+    <link rel="stylesheet" href="/eshop/css/niceselect.css">
+	<!-- Animate CSS -->
+    <link rel="stylesheet" href="/eshop/css/animate.css">
+	<!-- Flex Slider CSS -->
+    <link rel="stylesheet" href="/eshop/css/flex-slider.min.css">
+	<!-- Owl Carousel -->
+    <link rel="stylesheet" href="/eshop/css/owl-carousel.css">
+	<!-- Slicknav -->
+    <link rel="stylesheet" href="/eshop/css/slicknav.min.css">
+	
+	<!-- Eshop StyleSheet -->
+	<link rel="stylesheet" href="/eshop/css/reset.css">
+	<link rel="stylesheet" href="/eshop/css/style.css">
+    <link rel="stylesheet" href="/eshop/css/responsive.css">
+
+	
+	
+</head>
+<body class="js">
+	
+	<!-- Preloader -->
+	<div class="preloader">
+		<div class="preloader-inner">
+			<div class="preloader-icon">
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	</div>
+	<!-- End Preloader -->
+
+
+	@include('templates.eshop.partials.header')
+
+    @include('templates.eshop.partials.slider')
+
+    @yield('content')
+
+
+    @include('templates.eshop.partials.footer')
+
+	<!-- Jquery -->
+    <script src="/eshop/js/jquery.min.js"></script>
+    <script src="/eshop/js/jquery-migrate-3.0.0.js"></script>
+	<script src="/eshop/js/jquery-ui.min.js"></script>
+	<!-- Popper JS -->
+	<script src="/eshop/js/popper.min.js"></script>
+	<!-- Bootstrap JS -->
+	<script src="/eshop/js/bootstrap.min.js"></script>
+	<!-- Slicknav JS -->
+	<script src="/eshop/js/slicknav.min.js"></script>
+	<!-- Owl Carousel JS -->
+	<script src="/eshop/js/owl-carousel.js"></script>
+	<!-- Magnific Popup JS -->
+	<script src="/eshop/js/magnific-popup.js"></script>
+	<!-- Waypoints JS -->
+	<script src="/eshop/js/waypoints.min.js"></script>
+	<!-- Countdown JS -->
+	<script src="/eshop/js/finalcountdown.min.js"></script>
+	<!-- Nice Select JS -->
+	<script src="/eshop/js/nicesellect.js"></script>
+	<!-- Flex Slider JS -->
+	<script src="/eshop/js/flex-slider.js"></script>
+	<!-- ScrollUp JS -->
+	<script src="/eshop/js/scrollup.js"></script>
+	<!-- Onepage Nav JS -->
+	<script src="/eshop/js/onepage-nav.min.js"></script>
+	<!-- Easing JS -->
+	<script src="/eshop/js/easing.js"></script>
+	<!-- Active JS -->
+	<script src="/eshop/js/active.js"></script>
+</body>
+</html>
